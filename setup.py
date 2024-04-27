b44f427c0bebe761826a4d5f5a33d0375a1731b9
Andy <andrzej.zahorski@gmail.com>
2024-04-27 03:45:12 +0200

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mkdocs-sample-plugin',
-    version='0.1.0',
+    version='0.1.1',
     description='A sample MkDocs plugin to add custom text to the markdown pages.',
     author='Andrzej Zahorski',
     author_email='andrzej.zahorski@gmail.com',
